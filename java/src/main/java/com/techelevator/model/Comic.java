225db7bb1e31962355044c81382fb0618a4f5b2c
Emily Galbraith <galbraithemily94@gmail.com>
2021-12-06 12:05:13 -0500

    Made it more organized

--- java/src/main/java/com/techelevator/model/Comic.java
+++ java/src/main/java/com/techelevator/model/Comic.java
@@ -20,54 +20,24 @@ public class Comic {
     @NotNull(message = "A series id is required.")
     private int seriesId;
 
-    public int getComicId() {
-        return comicId;
-    }
+    public int getComicId() { return comicId; }
+    public void setComicId(int comicId) { this.comicId = comicId; }
 
-    public void setComicId(int comicId) {
-        this.comicId = comicId;
-    }
+    public String getComicName() { return comicName; }
+    public void setComicName(String comicName) { this.comicName = comicName; }
 
-    public String getComicName() {
-        return comicName;
-    }
-
-    public void setComicName(String comicName) {
-        this.comicName = comicName;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
 
     public String getImgUrl() { return imgUrl; }
     public void setImgUrl(String imgUrl) { this.imgUrl = imgUrl; }
 
-    public String getReleaseDate() {
-        return releaseDate;
-    }
+    public String getReleaseDate() { return releaseDate; }
+    public void setReleaseDate(String releaseDate) { this.releaseDate = releaseDate; }
 
-    public void setReleaseDate(String releaseDate) {
-        this.releaseDate = releaseDate;
-    }
+    public int getPublisherId() { return publisherId; }
+    public void setPublisherId(int publisherId) { this.publisherId = publisherId; }
 
-    public int getPublisherId() {
-        return publisherId;
-    }
-
-    public void setPublisherId(int publisherId) {
-        this.publisherId = publisherId;
-    }
-
-    public int getSeriesId() {
-        return seriesId;
-    }
-
-    public void setSeriesId(int seriesId) {
-        this.seriesId = seriesId;
-    }
+    public int getSeriesId() { return seriesId; }
+    public void setSeriesId(int seriesId) { this.seriesId = seriesId; }
 }
