0ee8bb3576b3b58e8bea65ea31be957f12265523
Ling Li-Fields <lunarasilvers@gmail.com>
2021-12-08 10:18:32 -0500

    comics display fixed

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -4,7 +4,9 @@
     <p>You must be authenticated to see this</p>
     <!-- <router-link v-bind:to="{name: 'all-collections'}">View All Collections</router-link> |  -->
     <router-link v-bind:to="{name: 'profile'}">View Profile</router-link>
-    <collection-list />
+    <div>
+      <collection-list />
+    </div>
   </div>
 </template>
 
