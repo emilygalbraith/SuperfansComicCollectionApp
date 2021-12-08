498f5d9949d94dd9d6049561b9b7f7d78e50d961
Emily Galbraith <galbraithemily94@gmail.com>
2021-12-08 07:17:14 -0500

    Added NewComic class to model

---
+++ java/src/main/java/com/techelevator/model/NewComic.java
@@ -0,0 +1,17 @@
+package com.techelevator.model;
+
+public class NewComic {
+
+    private Comic comic;
+    private String publisher;
+    private String series;
+
+    public Comic getComic() { return comic; }
+    public void setComic(Comic comic) { this.comic = comic; }
+
+    public String getPublisher() { return publisher; }
+    public void setPublisher(String publisher) { this.publisher = publisher; }
+
+    public String getSeries() { return series; }
+    public void setSeries(String series) { this.series = series; }
+}
