f19286453c45ff1f9ba2f7a7fcfe74b2ecf0df87
Emily Galbraith <galbraithemily94@gmail.com>
2021-12-10 13:47:15 -0500

    Added superhero class

---
+++ java/src/main/java/com/techelevator/model/Superhero.java
@@ -0,0 +1,13 @@
+package com.techelevator.model;
+
+public class Superhero {
+
+    private int superheroId;
+    private String superheroName;
+
+    public int getSuperheroId() { return superheroId; }
+    public void setSuperheroId(int superheroId) { this.superheroId = superheroId; }
+
+    public String getSuperheroName() { return superheroName; }
+    public void setSuperheroName(String superheroName) { this.superheroName = superheroName; }
+}
