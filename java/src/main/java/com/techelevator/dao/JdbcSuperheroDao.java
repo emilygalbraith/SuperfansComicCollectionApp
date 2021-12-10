9ef1b08d8ae56601a690232954738f98ea998abc
Emily Galbraith <galbraithemily94@gmail.com>
2021-12-10 13:56:59 -0500

    Created SuperheroDao and JdbcSuperheroDao

---
+++ java/src/main/java/com/techelevator/dao/JdbcSuperheroDao.java
@@ -0,0 +1,37 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Comic;
+import com.techelevator.model.Superhero;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class JdbcSuperheroDao implements SuperheroDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcSuperheroDao(JdbcTemplate jdbcTemplate) { this.jdbcTemplate = jdbcTemplate; }
+
+    @Override
+    public List<Superhero> listAllSuperheroes() {
+        List<Superhero> superheroList = new ArrayList<>();
+        String sql = "SELECT * FROM superheroes";
+        SqlRowSet result = jdbcTemplate.queryForRowSet(sql);
+        while(result.next()) {
+            superheroList.add(mapRowToSuperhero(result));
+        }
+        return superheroList;
+    }
+
+
+    private Superhero mapRowToSuperhero(SqlRowSet rs) {
+        Superhero superhero = new Superhero();
+        superhero.setSuperheroId(rs.getInt("superhero_id"));
+        superhero.setSuperheroName(rs.getString("superhero"));
+        return superhero;
+    }
+}
---
+++ java/src/main/java/com/techelevator/dao/SuperheroDao.java
@@ -0,0 +1,11 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Superhero;
+
+import java.util.List;
+
+public interface SuperheroDao {
+
+    List<Superhero> listAllSuperheroes();
+
+}
