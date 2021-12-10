1085a37fa55144c31bc14366121350327d92536a
Emily Galbraith <galbraithemily94@gmail.com>
2021-12-10 14:43:19 -0500

    Added method to ComicCuratorService that returns a list of SuperheroStats to track how often each hero appears in a users collections of comics

--- java/src/main/java/com/techelevator/model/SuperheroStat.java
+++ java/src/main/java/com/techelevator/model/SuperheroStat.java
@@ -5,6 +5,11 @@ public class SuperheroStat {
     private String heroName;
     private int occurrences;
 
+    public SuperheroStat(String heroName, int occurrences) {
+        this.heroName = heroName;
+        this.occurrences = occurrences;
+    }
+
     public String getHeroName() { return heroName; }
     public void setHeroName(String heroName) { this.heroName = heroName; }
 
