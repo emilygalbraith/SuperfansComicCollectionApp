6ae121251cf6f946c7ac58d0c2e5a04d3e76e2cf
Emily Galbraith <galbraithemily94@gmail.com>
2021-12-12 16:13:02 -0500

    Added some styles to CollectionStatistics

--- vue/src/components/CollectionStatistics.vue
+++ vue/src/components/CollectionStatistics.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="statistics-display">
     <div id="superhero">
-      <p>Top 5 Superheroes in Collection:</p>
+      <h3>Top 5 Superheroes in Collection:</h3>
       <p v-for="superhero in superheroStats" v-bind:key="superhero.heroName">
         {{ superhero.heroName }}: {{ superhero.occurrences }}
       </p>
     </div>
     <div id="publisher">
-      <p>Top 5 Publishers in Collection:</p>
+      <h3>Top 5 Publishers in Collection:</h3>
       <p v-for="publisher in publisherStats" v-bind:key="publisher.publisherName">
         {{ publisher.publisherName }}: {{ publisher.occurrences }}
       </p>
     </div>
     <div id="series">
-      <p>Top 5 Series in Collection:</p>
+      <h3>Top 5 Series in Collection:</h3>
       <p v-for="series in seriesStats" v-bind:key="series.seriesName">
         {{ series.seriesName }}: {{ series.occurrences }}
       </p>
@@ -78,4 +78,16 @@ export default {
   display: flex;
   flex-direction: column;
 }
+#superhero {
+    margin-bottom: 40px;
+}
+#publisher {
+    margin-bottom: 40px;
+}
+h3 {
+    color: white;
+    text-decoration: underline;
+    font-weight: bold;
+}
+
 </style>
