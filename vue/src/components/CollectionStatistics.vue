7d89b7cb71661188210c6b1e5abf8d53d36f3b45
Emily Galbraith <galbraithemily94@gmail.com>
2021-12-12 16:06:32 -0500

    Added statistics to the side of collection details

--- vue/src/components/CollectionStatistics.vue
+++ vue/src/components/CollectionStatistics.vue
@@ -1,62 +1,81 @@
 <template>
   <div id="statistics-display">
-      <div id="superhero">
-          <p v-for="superhero in superheroStats" v-bind:key="superhero.heroName">{{superhero.heroName}}: {{superhero.occurrences}}</p>
-      </div>
-      <div id="publisher"></div>
-      <div id="series"></div>
+    <div id="superhero">
+      <p>Top 5 Superheroes in Collection:</p>
+      <p v-for="superhero in superheroStats" v-bind:key="superhero.heroName">
+        {{ superhero.heroName }}: {{ superhero.occurrences }}
+      </p>
+    </div>
+    <div id="publisher">
+      <p>Top 5 Publishers in Collection:</p>
+      <p v-for="publisher in publisherStats" v-bind:key="publisher.publisherName">
+        {{ publisher.publisherName }}: {{ publisher.occurrences }}
+      </p>
+    </div>
+    <div id="series">
+      <p>Top 5 Series in Collection:</p>
+      <p v-for="series in seriesStats" v-bind:key="series.seriesName">
+        {{ series.seriesName }}: {{ series.occurrences }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-import comicService from '@/services/ComicService.js'
+import comicService from "@/services/ComicService.js";
 export default {
-    name: "collection-statistics",
-    data() {
-        return {
-            superheroStats: [],
-            publisherStats: [],
-            seriesStats: [],
-        }
+  name: "collection-statistics",
+  data() {
+    return {
+      superheroStats: [],
+      publisherStats: [],
+      seriesStats: [],
+    };
+  },
+  created() {
+    this.getSuperheroStats();
+    this.getPublisherStats();
+    this.getSeriesStats();
+  },
+  methods: {
+    getSuperheroStats() {
+      comicService
+        .getSuperheroCollectionStats(this.$route.params.collectionId)
+        .then((response) => {
+          if (response.status === 200) {
+            this.superheroStats = response.data;
+            this.superheroStats.sort((a, b) => b.occurrences - a.occurrences);
+            this.superheroStats.splice(6);
+          }
+        });
     },
-    created() {
-        this.getSuperheroStats();
-        this.getPublisherStats();
-        this.getSeriesStats();
+
+    getPublisherStats() {
+      comicService
+        .getPublisherCollectionStats(this.$route.params.collectionId)
+        .then((response) => {
+          if (response.status === 200) {
+            this.publisherStats = response.data;
+          }
+        });
     },
-    methods: {
-        getSuperheroStats() {
-            comicService.getSuperheroCollectionStats(this.$route.params.collectionId).then( response => {
-                if(response.status === 200) {
-                    this.superheroStats = response.data;
-                    this.superheroStats.sort( (a, b) => b.occurrences - a.occurrences);
-                }
-            });
-        },
 
-        getPublisherStats() {
-            comicService.getPublisherCollectionStats(this.$route.params.collectionId).then( response => {
-                if(response.status === 200) {
-                    this.publisherStats = response.data;
-                }
-            });
-        },
-
-        getSeriesStats() {
-            comicService.getSeriesCollectionStats(this.$route.params.collectionId).then( response => {
-                if(response.status === 200) {
-                    this.seriesStats = response.data;
-                }
-            });
-        }
-    }
-
-}
+    getSeriesStats() {
+      comicService
+        .getSeriesCollectionStats(this.$route.params.collectionId)
+        .then((response) => {
+          if (response.status === 200) {
+            this.seriesStats = response.data;
+          }
+        });
+    },
+  },
+};
 </script>
 
 <style>
-#statistics-display{
-    display: flex;
-    flex-direction: column;
+#statistics-display {
+  display: flex;
+  flex-direction: column;
 }
 </style>
