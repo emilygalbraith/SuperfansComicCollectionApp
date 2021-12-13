943bb1351f2f73cdc93276cb8d15f561484cf2f6
Emily Galbraith <galbraithemily94@gmail.com>
2021-12-13 13:33:51 -0500

    Added button to add comic for to conditionally display the form

--- vue/src/components/AddComic.vue
+++ vue/src/components/AddComic.vue
@@ -1,47 +1,42 @@
 <template>
-  <form v-on:submit.prevent>
-    <div>
-      <label for="comic-name">Comic Name</label>
-      <input type="text" v-model="newComic.comic.comicName" />
-    </div>
-    <div>
-      <label for="author">Author</label>
-      <input type="text" v-model="newComic.comic.author" />
-    </div>
-    <div>
-      <label for="image">Image URL</label>
-      <input type="text" v-model="newComic.comic.image"/>
-    </div>
-    <div>
-      <label for="releaseDate">Release Date</label>
-      <input type="date" v-model="newComic.comic.releaseDate" />
-    </div>
-    <div>
-      <label for="publisher">Publisher</label>
-      <input type="text" v-model="newComic.publisher" />
-    </div>
-    <div>
-      <label for="series">Series</label>
-      <input type="text" v-model="newComic.series" />
-    </div>
-    <label for="superheroes">Please Check All Superheroes In Comic</label><br/>
-    <span v-for="superhero in superheroList" :key="superhero.superheroId">
-      <input type="checkbox" :id="superhero.superheroName" v-model="newComic.superheroes" :value="superhero.superheroName"> 
-      <label :for="superhero.superheroId">{{superhero.superheroName}}</label>
-    </span>
-    <div>
-      <label for="add-comic"
-        >Would you like to add another comic to this collection now?</label
-      >
-      <label for="yes">Yes</label>
-      <input type="radio" id="yes" v-bind:value="true" v-model="addComic" />
-      <label for="no">No</label>
-      <input type="radio" id="no" v-bind:value="false" v-model="addComic" />
-    </div>
-    <div>
-      <button type="submit" v-on:click="saveComic()">Add Comic</button>
-    </div>
-  </form>
+  <div>
+    <button v-on:click="toggleShow()" v-if="!show">Add New Comic To Collection</button>
+    <form v-on:submit.prevent v-if="show">
+      <div>
+        <label for="comic-name">Comic Name: </label>
+        <input type="text" v-model="newComic.comic.comicName" />
+        <label for="author">Author: </label>
+        <input type="text" v-model="newComic.comic.author" />
+        <label for="image">Image URL: </label>
+        <input type="text" v-model="newComic.comic.image" />
+      </div>
+      <div>
+        <label for="releaseDate">Release Date: </label>
+        <input type="date" v-model="newComic.comic.releaseDate" />
+
+        <label for="publisher">Publisher: </label>
+        <input type="text" v-model="newComic.publisher" />
+        <label for="series">Series: </label>
+        <input type="text" v-model="newComic.series" />
+      </div>
+      <label for="superheroes">Please Check All Superheroes In Comic</label
+      ><br />
+      <span v-for="superhero in superheroList" :key="superhero.superheroId">
+        <input
+          type="checkbox"
+          :id="superhero.superheroName"
+          v-model="newComic.superheroes"
+          :value="superhero.superheroName"
+        />
+        <label :for="superhero.superheroId">{{
+          superhero.superheroName
+        }}</label>
+      </span>
+      <div>
+        <button type="submit" v-on:click="saveComic()">Add Comic</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -59,10 +54,11 @@ export default {
         },
         publisher: "",
         series: "",
-        superheroes: []
+        superheroes: [],
       },
       addComic: "",
-      superheroList: []
+      superheroList: [],
+      show: false
     };
   },
   created() {
@@ -70,28 +66,21 @@ export default {
   },
   methods: {
     getListOfSuperheroes() {
-      comicService.getAllSuperheroes().then( response => {
-        if(response.status === 200) {
+      comicService.getAllSuperheroes().then((response) => {
+        if (response.status === 200) {
           this.superheroList = response.data;
         }
       });
     },
     saveComic() {
       const token = this.$store.state.token;
-    //   this.newComic.comic.image = this.$refs.fileInput.files[0];
+      //   this.newComic.comic.image = this.$refs.fileInput.files[0];
       const collectionId = this.$route.params.collectionId;
       comicService
         .addComic(this.newComic, collectionId, token)
         .then((response) => {
           if (response.status === 201) {
-            if (this.addComic == "true") {
-              this.$router.push({
-                name: "add-comic-form",
-                params: { collectionId: collectionId },
-              });
-            } else {
-              this.$router.push({ name: "my-collections" });
-            }
+            this.toggleShow();
           }
         })
         .catch((error) => {
@@ -104,6 +93,13 @@ export default {
           }
         });
     },
+    toggleShow() {
+      if(this.show) {
+        this.show = false;
+      } else {
+        this.show = true;
+      }
+    }
   },
 };
 </script>
@@ -116,6 +112,10 @@ span {
   margin-right: 20px;
 }
 form {
+  margin-top: 50px;
+  margin-right: 30px;
+}
+input {
   margin-right: 30px;
 }
 </style>
