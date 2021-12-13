186d9c52daf4c7c5411297945cfd43708823a523
Emily Galbraith <galbraithemily94@gmail.com>
2021-12-13 11:06:36 -0500

    Working on styling profile to look like Maria's mock up

--- vue/src/components/MyProfile.vue
+++ vue/src/components/MyProfile.vue
@@ -2,7 +2,8 @@
     <div   id="profile" >
         <h2>{{$store.state.user.username}}</h2>
         <!-- <img :src="image" />
-        <input type="file" id="file" ref="fileInput"/>  not sure how to work with profile photo yet-->  
+        <input type="file" id="file" ref="fileInput"/>  not sure how to work with profile photo yet-->
+        <h3>{{userType}}</h3>  
         <h3>All My Collections:</h3> 
         <collection-list />
         <router-link :to="{ name: 'create-collection'}" id="create">
@@ -12,7 +13,8 @@
 </template>
 
 <script>
-import CollectionList from './CollectionList.vue'
+import CollectionList from './CollectionList.vue';
+import ComicService from '../services/ComicService';
 
 export default {
   components: { CollectionList },
@@ -20,6 +22,25 @@ export default {
     data() {
         return {
             // image: this.$refs.fileInput.files[0]
+            userType: ""
+        }
+    },
+    created() {
+        this.getUserType();
+    },
+    methods: {
+        getUserType() {
+            const currentUser = this.$store.state.user
+            const usersCollections = ComicService.getCollectionByUserId(this.$route.params(currentUser.id));
+            let count = 0;
+            for(let i = 0; i < usersCollections.length; i++) {
+                count++;
+            }
+            if(count >= 0 && count <= 3) {
+                this.userType = count + " collections | Standard ";
+            } else if (count > 3) {
+                this.userType = count + " collections | Premium ";
+            }
         }
     }
 }
