eab57b56d2bf792fcf471fe4ebc36623fc40967c
Emily Galbraith <galbraithemily94@gmail.com>
2021-12-14 11:27:14 -0500

    Added FLIP_SHOW, true to the profile view so that it doesn't stop showing users collections

--- vue/src/views/Profile.vue
+++ vue/src/views/Profile.vue
@@ -1,16 +1,17 @@
 <template>
-    <my-profile/>
+  <my-profile />
 </template>
 
 <script>
-import MyProfile from '../components/MyProfile.vue'
+import MyProfile from "../components/MyProfile.vue";
 
 export default {
   components: { MyProfile },
-
-}
+  created() {
+    this.$store.commit("FLIP_SHOW", true);
+  },
+};
 </script>
 
 <style>
-
 </style>
