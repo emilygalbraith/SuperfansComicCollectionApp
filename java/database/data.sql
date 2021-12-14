43068261041608f23423e26a9c426d5a1f615f00
Corbin Outten <panther7303@gmail.com>
2021-12-14 11:37:30 -0500

    Submitting Work Done

--- java/database/data.sql
+++ java/database/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO publishers (publisher_name)
-VALUES ('Marvel'), ('DC');
+VALUES ('Marvel'), ('DC'), ('Image');
 
 INSERT INTO series (series_name)
-VALUES ('Empyre'), ('Spider-Man'), ('Marvel Knigths'), ('Storm'), ('Batman'), ('Wonder Woman');
+VALUES ('Empyre'), ('Spider-Man'), ('Marvel Knigths'), ('Storm'), ('Batman'), ('Wonder Woman'), ('Superman'), ('The Flash'), ('Justice League'), ('Spawn');
 
 INSERT INTO comics (comic_name, author, image, release_date, publisher_id, series_id)
 VALUES ('Empyre #1', 'Al Ewing', 'http://i.annihil.us/u/prod/marvel/i/mg/c/80/5e3d7536c8ada/portrait_xlarge.jpg', '2020-07-15', (SELECT publisher_id FROM publishers WHERE publisher_name='Marvel'), (SELECT series_id FROM series WHERE series_name='Empyre')),
@@ -10,12 +10,16 @@ VALUES ('Empyre #1', 'Al Ewing', 'http://i.annihil.us/u/prod/marvel/i/mg/c/80/5e
        ('Official Handbook of the Marvel Universe', 'Eric J. Moreels, Barry Reese, Ronald Byrd, Jeff Christiansen', 'http://i.annihil.us/u/prod/marvel/i/mg/9/30/4bc64df4105b9/portrait_xlarge.jpg', '2004-12-31', (SELECT publisher_id FROM publishers WHERE publisher_name='Marvel'), (SELECT series_id FROM series WHERE series_name='Marvel Knigths')),
        ('Storm Vol 2 #1', 'Eric Jerome Dickey', 'http://i.annihil.us/u/prod/marvel/i/mg/c/80/4bc5fe7a308d7/portrait_xlarge.jpg', '2008-01-16', (SELECT publisher_id FROM publishers WHERE publisher_name='Marvel'), (SELECT series_id FROM series WHERE series_name='Storm')),
        ('Batman: Knightfall Vol. 1', 'Alan Grant, Chuck Dixon, Doug Moench, Jo Duffy', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWXiPXpET1LjUQcdI2eL9YSJZQy0BHg4UxpA&usqp=CAU', '2012-05-01', (SELECT publisher_id FROM publishers WHERE publisher_name='DC'), (SELECT series_id FROM series WHERE series_name='Batman')),
-       ('Wonder Woman Vol. 5', 'Greg Rucka, Liam Sharp, Nicola Scott, James Robinson, Steve Orlando, G. Willow Wilson', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDPgzvwrIhUCOUwypmi_BX84djp8_ngPsGxQ&usqp=CAU', '2016-08-01', (SELECT publisher_id FROM publishers WHERE publisher_name='DC'), (SELECT series_id FROM series WHERE series_name='Wonder Woman'));
+       ('Wonder Woman Vol. 5', 'Greg Rucka, Liam Sharp, Nicola Scott, James Robinson, Steve Orlando, G. Willow Wilson', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDPgzvwrIhUCOUwypmi_BX84djp8_ngPsGxQ&usqp=CAU', '2016-08-01', (SELECT publisher_id FROM publishers WHERE publisher_name='DC'), (SELECT series_id FROM series WHERE series_name='Wonder Woman')),
+       ('Superman Vol. 5', 'Brian Michael Bendis, Ivan Reis', 'https://pbs.twimg.com/media/Dh2ihdcVQAAPmQl.jpg:large', '2018-09-01', (SELECT publisher_id FROM publishers WHERE publisher_name='DC'), (SELECT series_id FROM series WHERE series_name='Superman')),
+       ('Flashpoint', 'Geoff Johns', 'https://upload.wikimedia.org/wikipedia/en/f/fa/Flashpoint_%28DC_Comics%29.png', '2011-08-01', (SELECT publisher_id FROM publishers WHERE publisher_name='DC'), (SELECT series_id FROM series WHERE series_name='The Flash')),
+       ('Justice League Vol. 1: Origin', 'Geoff Johns', 'https://m.media-amazon.com/images/P/B008J2GFV4.01._SCLZZZZZZZ_SX500_.jpg', '2013-02-05', (SELECT publisher_id FROM publishers WHERE publisher_name='DC'), (SELECT series_id FROM series WHERE series_name='Justice League')),
+       ('Spawn #1', 'Todd Mcfarlane', 'https://images-na.ssl-images-amazon.com/images/I/61XaC4WLwxL._SX326_BO1,204,203,200_.jpg', '1992-01-01', (SELECT publisher_id FROM publishers WHERE publisher_name='Image'), (SELECT series_id FROM series WHERE series_name='Spawn'));
 
 INSERT INTO superheroes (superhero)
 VALUES ('Spider-Man'), ('Storm'), ('Black Panther'), ('Human Torch'), ('The Thing'), ('Invisible Woman'), ('Mister Fantastic'), ('Iron Man'), ('Captain America'), ('Thor'), ('Captain Marvel'), ('Ghost Rider'), ('She-Hulk'), 
        ('Blade'), ('Luke Cage'), ('Cloak'), ('Daredevil'), ('District X'), ('Dr. Strange'), ('Nick Fury'), ('Iron Fist'), ('Madrox'), ('Man-Thing'), ('Marvel Knights'), ('Moon Knight'), ('Punisher'), ('Shang-Chi'), ('White Tiger'),
-       ('Batman'), ('Robin'), ('Nightwing'), ('Azrael'), ('Wonder Woman');
+       ('Batman'), ('Robin'), ('Nightwing'), ('Azrael'), ('Wonder Woman'), ('Superman'), ('Flash'), ('Cyborg'), ('Booster Gold'), ('Aquaman'), ('Green Lantern'), ('Spawn');
 
 INSERT INTO comic_superheroes (comic_id, superhero_id)
 VALUES ((SELECT comic_id FROM comics WHERE comic_name='Empyre #1'), (SELECT superhero_id FROM superheroes WHERE superhero='Human Torch')),
@@ -53,18 +57,47 @@ VALUES ((SELECT comic_id FROM comics WHERE comic_name='Empyre #1'), (SELECT supe
        ((SELECT comic_id FROM comics WHERE comic_name='Batman: Knightfall Vol. 1'), (SELECT superhero_id FROM superheroes WHERE superhero='Robin')),
        ((SELECT comic_id FROM comics WHERE comic_name='Batman: Knightfall Vol. 1'), (SELECT superhero_id FROM superheroes WHERE superhero='Nightwing')),
        ((SELECT comic_id FROM comics WHERE comic_name='Batman: Knightfall Vol. 1'), (SELECT superhero_id FROM superheroes WHERE superhero='Azrael')),
-       ((SELECT comic_id FROM comics WHERE comic_name='Wonder Woman Vol. 5'), (SELECT superhero_id FROM superheroes WHERE superhero='Wonder Woman'));
+       ((SELECT comic_id FROM comics WHERE comic_name='Wonder Woman Vol. 5'), (SELECT superhero_id FROM superheroes WHERE superhero='Wonder Woman')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Superman Vol. 5'), (SELECT superhero_id FROM superheroes WHERE superhero='Superman')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Flashpoint'), (SELECT superhero_id FROM superheroes WHERE superhero='Flash')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Flashpoint'), (SELECT superhero_id FROM superheroes WHERE superhero='Batman')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Flashpoint'), (SELECT superhero_id FROM superheroes WHERE superhero='Cyborg')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Flashpoint'), (SELECT superhero_id FROM superheroes WHERE superhero='Booster Gold')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Flashpoint'), (SELECT superhero_id FROM superheroes WHERE superhero='Wonder Woman')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Flashpoint'), (SELECT superhero_id FROM superheroes WHERE superhero='Aquaman')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Justice League Vol. 1: Origin'), (SELECT superhero_id FROM superheroes WHERE superhero='Superman')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Justice League Vol. 1: Origin'), (SELECT superhero_id FROM superheroes WHERE superhero='Wonder Woman')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Justice League Vol. 1: Origin'), (SELECT superhero_id FROM superheroes WHERE superhero='Batman')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Justice League Vol. 1: Origin'), (SELECT superhero_id FROM superheroes WHERE superhero='Aquaman')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Justice League Vol. 1: Origin'), (SELECT superhero_id FROM superheroes WHERE superhero='Flash')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Justice League Vol. 1: Origin'), (SELECT superhero_id FROM superheroes WHERE superhero='Cyborg')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Justice League Vol. 1: Origin'), (SELECT superhero_id FROM superheroes WHERE superhero='Green Lantern')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Spawn #1'), (SELECT superhero_id FROM superheroes WHERE superhero='Spawn'));
 
 INSERT INTO collections (collection_name, private, user_id)
-VALUES ('User Collection 1', FALSE, (SELECT user_id FROM users WHERE username='testuser')),
-       ('User Collection 2', FALSE, (SELECT user_id FROM users WHERE username='DCFan'));
+VALUES ('Marvel Collection', FALSE, (SELECT user_id FROM users WHERE username='testuser')),
+       ('DC Collection', FALSE, (SELECT user_id FROM users WHERE username='DCFan')),
+       ('Image Collection', FALSE, (SELECT user_id FROM users WHERE username='ImageFan')),
+       ('Super Collection', FALSE, (SELECT user_id FROM users WHERE username='SuperFan100'));
 
 INSERT INTO collection_comic (comic_id, collection_id)
-VALUES ((SELECT comic_id FROM comics WHERE comic_name='Empyre #1'), (SELECT collection_id FROM collections WHERE collection_name='User Collection 1')),
-       ((SELECT comic_id FROM comics WHERE comic_name='Marvel Age Spider-Man #8'), (SELECT collection_id FROM collections WHERE collection_name='User Collection 1')),
-       ((SELECT comic_id FROM comics WHERE comic_name='Official Handbook of the Marvel Universe'), (SELECT collection_id FROM collections WHERE collection_name='User Collection 1')),
-       ((SELECT comic_id FROM comics WHERE comic_name='Storm Vol 2 #1'), (SELECT collection_id FROM collections WHERE collection_name='User Collection 1')),
-       ((SELECT comic_id FROM comics WHERE comic_name='Batman: Knightfall Vol. 1'), (SELECT collection_id FROM collections WHERE collection_name='User Collection 2')),
-       ((SELECT comic_id FROM comics WHERE comic_name='Wonder Woman Vol. 5'), (SELECT collection_id FROM collections WHERE collection_name='User Collection 2'));
-
-
+VALUES ((SELECT comic_id FROM comics WHERE comic_name='Empyre #1'), (SELECT collection_id FROM collections WHERE collection_name='Marvel Collection')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Marvel Age Spider-Man #8'), (SELECT collection_id FROM collections WHERE collection_name='Marvel Collection')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Official Handbook of the Marvel Universe'), (SELECT collection_id FROM collections WHERE collection_name='Marvel Collection')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Storm Vol 2 #1'), (SELECT collection_id FROM collections WHERE collection_name='Marvel Collection')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Batman: Knightfall Vol. 1'), (SELECT collection_id FROM collections WHERE collection_name='DC Collection')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Wonder Woman Vol. 5'), (SELECT collection_id FROM collections WHERE collection_name='DC Collection')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Superman Vol. 5'), (SELECT collection_id FROM collections WHERE collection_name='DC Collection')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Flashpoint'), (SELECT collection_id FROM collections WHERE collection_name='DC Collection')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Justice League Vol. 1: Origin'), (SELECT collection_id FROM collections WHERE collection_name='DC Collection')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Spawn #1'), (SELECT collection_id FROM collections WHERE collection_name='Image Collection')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Empyre #1'), (SELECT collection_id FROM collections WHERE collection_name='Super Collection')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Marvel Age Spider-Man #8'), (SELECT collection_id FROM collections WHERE collection_name='Super Collection')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Official Handbook of the Marvel Universe'), (SELECT collection_id FROM collections WHERE collection_name='Super Collection')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Storm Vol 2 #1'), (SELECT collection_id FROM collections WHERE collection_name='Super Collection')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Batman: Knightfall Vol. 1'), (SELECT collection_id FROM collections WHERE collection_name='Super Collection')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Wonder Woman Vol. 5'), (SELECT collection_id FROM collections WHERE collection_name='Super Collection')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Superman Vol. 5'), (SELECT collection_id FROM collections WHERE collection_name='Super Collection')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Flashpoint'), (SELECT collection_id FROM collections WHERE collection_name='Super Collection')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Justice League Vol. 1: Origin'), (SELECT collection_id FROM collections WHERE collection_name='Super Collection')),
+       ((SELECT comic_id FROM comics WHERE comic_name='Spawn #1'), (SELECT collection_id FROM collections WHERE collection_name='Super Collection'));
